3c494cebddb9e339fc1c982a215c337ed2e4f53a
Ye Chen <ye.chen@ihandysoft.com>
2018-08-18 19:49:52 +0800

    try to add multi sub folder support, not success now

--- log-cleanup/airflow-log-cleanup.py
+++ log-cleanup/airflow-log-cleanup.py
@@ -7,7 +7,10 @@ import os
 """
 A maintenance workflow that you can deploy into Airflow to periodically clean out the task logs to avoid those getting too big.
 
-airflow trigger_dag --conf '{"maxLogAgeInDays":30}' airflow-log-cleanup
+airflow trigger_dag airflow-log-cleanup
+
+Variable:
+    max_log_age_in_days: {"dag1": 30, "dag2": 7, "scheduler": 2, ...}
 
 --conf options:
     maxLogAgeInDays:<INT> - Optional
@@ -20,7 +23,7 @@ BASE_LOG_FOLDER = conf.get("core", "BASE_LOG_FOLDER")
 SCHEDULE_INTERVAL = "@daily"        # How often to Run. @daily - Once a day at Midnight
 DAG_OWNER_NAME = "operations"       # Who is listed as the owner of this DAG in the Airflow Web Server
 ALERT_EMAIL_ADDRESSES = []          # List of email address to send email alerts to if this job fails
-DEFAULT_MAX_LOG_AGE_IN_DAYS = Variable.get("max_log_age_in_days", 30)    # Length to retain the log files if not already provided in the conf. If this is set to 30, the job will remove those files that are 30 days old or odler
+DEFAULT_MAX_LOG_AGE_IN_DAYS = Variable.get("max_log_age_in_days", default_var={}, deserialize_json=True)    # Length to retain the log files for each dag.
 ENABLE_DELETE = True                # Whether the job should delete the logs or not. Included if you want to temporarily avoid deleting the logs
 NUMBER_OF_WORKERS = 1               # The number of worker nodes you have in Airflow. Will attempt to run this process for however many workers there are so that each worker gets its logs cleared.
 
@@ -40,24 +43,34 @@ log_cleanup = """
 echo "Getting Configurations..."
 BASE_LOG_FOLDER='""" + BASE_LOG_FOLDER + """'
 MAX_LOG_AGE_IN_DAYS="{{dag_run.conf.maxLogAgeInDays}}"
-if [ "${MAX_LOG_AGE_IN_DAYS}" == "" ]; then
-    echo "maxLogAgeInDays conf variable isn't included. Using Default '""" + str(DEFAULT_MAX_LOG_AGE_IN_DAYS) + """'."
-    MAX_LOG_AGE_IN_DAYS='""" + str(DEFAULT_MAX_LOG_AGE_IN_DAYS) + """'
-fi
 ENABLE_DELETE=""" + str("true" if ENABLE_DELETE else "false") + """
 echo "Finished Getting Configurations"
 echo ""
 
 echo "Configurations:"
 echo "BASE_LOG_FOLDER:      '${BASE_LOG_FOLDER}'"
-echo "MAX_LOG_AGE_IN_DAYS:  '${MAX_LOG_AGE_IN_DAYS}'"
+"""
+
+for key, day in DEFAULT_MAX_LOG_AGE_IN_DAYS.items():
+    log_cleanup += """
+        echo "LOG_SUB_FOLDER: '${key}', MAX_AGE_IN_DAYS: ${day}"
+    """
+
+log_cleanup += """
 echo "ENABLE_DELETE:        '${ENABLE_DELETE}'"
 echo ""
 
 echo "Running Cleanup Process..."
-FIND_STATEMENT="find ${BASE_LOG_FOLDER}/*/* -type f -mtime +${MAX_LOG_AGE_IN_DAYS}"
-echo "Executing Find Statement: ${FIND_STATEMENT}"
-FILES_MARKED_FOR_DELETE=`eval ${FIND_STATEMENT}`
+FILES_MARKED_FOR_DELETE=""
+"""
+for key, day in DEFAULT_MAX_LOG_AGE_IN_DAYS.items():
+    log_cleanup+="""
+        FIND_STATEMENT="find ${BASE_LOG_FOLDER}/${key}/*/* -type f -mtime +${day}"
+        echo "Executing Find Statement: ${FIND_STATEMENT}"
+        FILES_MARKED_FOR_DELETE="FILES_MARKED_FOR_DELETE `eval ${FIND_STATEMENT}`"
+        """
+        
+log_cleanup +="""        
 echo "Process will be Deleting the following directories:"
 echo "${FILES_MARKED_FOR_DELETE}"
 echo "Process will be Deleting `echo "${FILES_MARKED_FOR_DELETE}" | grep -v '^$' | wc -l ` file(s)"     # "grep -v '^$'" - removes empty lines. "wc -l" - Counts the number of lines
